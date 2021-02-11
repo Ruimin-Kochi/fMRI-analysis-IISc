03be16e8fe00c424bb07e72e64d414ac7906abbc
Nabarun Sarkar <eucleodo@Nabaruns-MacBook-Air.local>
2021-02-11 17:38:40 +0530

    modify batch script

--- slurm_fmriprep.sh
+++ slurm_fmriprep.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 #SBATCH --job-name=fmriprep    # Job name
-#SBATCH --array=1-64%4
+#SBATCH --array=1-176%3
 #SBATCH --ntasks=1         # Run on a single CPU
-#SBATCH --time=12:00:00  # Time limit hrs:min:sec
+#SBATCH --time=02:00:00  # Time limit hrs:min:sec
 #SBATCH -o log/%x-%A-%a.out
 ##SBATCH --gres=gpu:1
 ##SBATCH --partition=q_1day-4G
 #SBATCH --partition=q2h_12h-32C
 pwd; hostname; date |tee result
 
-SCRATCH_DIR="/scratch/nabaruns"
-BIDS_DIR="$SCRATCH_DIR/BIDS"
-OUT_DIR="$HOME/fmriprep2"
-WORK_DIR="$SCRATCH_DIR/work"
+SCRATCH_DIR="/scratch/nbrn"
+BIDS_DIR="$HOME/BIDS"
+OUT_DIR="$HOME/fmriprep4"
+WORK_DIR="$HOME/work"
+
+mkdir -p $BIDS_DIR && mkdir -p $OUT_DIR && mkdir -p $WORK_DIR
 
 # subject=$( ls /scratch/nabaruns/BIDS/ | sed "${SLURM_ARRAY_TASK_ID}q;d" )
 subject=$( cat $HOME/participants.tsv | sed "${SLURM_ARRAY_TASK_ID}q;d" )
 sids=$( echo $subject | sed "s/_[0-9]*//" )
 
+echo "Making $subject output folder"
+mkdir -p $BIDS_DIR/$subject && mkdir -p $OUT_DIR/$subject && mkdir -p $WORK_DIR/$subject
+
 echo "Copying $subject BIDS"
 copy_cmd="scp -r nabarun@10.36.17.186:'/media/varsha/Seagate\ Backup\ Plus\ Drive/MRI_DEVARAJAR/RADC_Dicom/BIDS/$subject' $BIDS_DIR/"
-# echo Commandline: $copy_cmd
-# scp -r nabarun@10.36.17.186:'/media/varsha/Seagate\ Backup\ Plus\ Drive/MRI_DEVARAJAR/RADC_Dicom/BIDS/$subject' $BIDS_DIR
+echo Commandline: $copy_cmd
+# scp -r nabarun@10.36.17.186:'/media/varsha/Seagate\ Backup\ Plus\ Drive/MRI_DEVARAJAR/RADC_Dicom/BIDS/$subject/*' $BIDS_DIR/$subject/
 eval $copy_cmd
 exitcode=$?
 if [ "$exitcode" -ne "0" ]
@@ -33,10 +38,7 @@ then
 	exit $exitcode
 fi
 
-echo "Making $subject output folder"
-mkdir $OUT_DIR/$subject
-mkdir $WORK_DIR/$subject
-cmd="nvidia-docker run --name $SLURM_JOB_ID --user $(id -u $USER):$(id -g $USER) --rm \
+cmd="docker run --init --name $SLURM_JOB_ID --user $(id -u $USER):$(id -g $USER) --rm \
 	-v $BIDS_DIR/$subject/:/data:ro \
 	-v $OUT_DIR/$subject/:/out \
 	-v $HOME/:/mhome/ \
@@ -46,7 +48,7 @@ cmd="nvidia-docker run --name $SLURM_JOB_ID --user $(id -u $USER):$(id -g $USER)
 	participant \
 	-w /work \
 	--participant_label $sids \
-	--fs-license-file /mhome/freesurfer.txt \
+	--fs-license-file /mhome/license.txt \
 	--fs-no-reconall --output-spaces MNI152NLin6Asym:res-2"
 # cmd="fmriprep-docker $BIDS_DIR/$subject/ $OUT_DIR/$subject/ --participant_label $sids --fs-license-file $HOME/freesurfer.txt --fs-no-reconall --output-spaces MNI152NLin6Asym:res-2"
 echo Running task ${SLURM_ARRAY_TASK_ID}
